54fe4fceb7e10e6279c0191d85d458ad3dabf942
dpiskac <dpiskac@gmail.com>
2011-04-25 16:38:51 -0500

    poop

--- source/ui.h
+++ source/ui.h
@@ -19,7 +19,7 @@ void ui_draw()
 	for (i = 0; i<2; i++)
 		for ( j = 0; j < 32; ++j)
 			bg1map[i * 32 + j ] = fontMap[1];
-	for (i = 2; i<32; i++)
+	for (i = 2; i<32; i++) 
 		for ( j = 0; j < 2; ++j)
 			bg1map[i * 32 + j ] = fontMap[1];
 	ui_updateStatus();
